89a20c047fe241b9023e0d2f97a6277b11075eeb
luciaaroca <lucia.aroca2510@gmail.com>
2025-12-13 12:59:44 +0100

    start/adminFront

---
+++ frontend/src/components/Main/AdminDashboard/AdminDashboad.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import {} from '../../../services/userServices'
+
+
+
+
+const AdminDashboard = () => {
+  
+  return <div>
+
+  </div>
+};
+
+export default AdminDashboard;
